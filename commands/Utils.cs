9244b867a495755a501ba1f761780babd962b428
hvu6902 <hoangvupantc@gmail.com>
2024-05-02 22:04:34 -0400

    Add project files.

---
+++ commands/Utils.cs
@@ -0,0 +1,99 @@
+﻿using Discord_App.other_command;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Messaging;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discord_App.commands
+{
+    internal class Utils : BaseCommandModule
+    {
+        [Command("test")]
+        public async Task FirstCommand(CommandContext context)
+        {
+            await context.Channel.SendMessageAsync($"Dit me may {context.User.Username}");
+        }
+
+        [Command("add")]
+        public async Task Add(CommandContext context, int number1, int number2)
+        {
+            int result = number1 + number2;
+            await context.Channel.SendMessageAsync(result.ToString());
+        }
+
+        [Command("embed")]
+        public async Task EmbedMsg(CommandContext context)
+        {
+            var msg = new DiscordEmbedBuilder
+            {
+                Title = "Thong bao",
+                Description = $"Me thang {context.User.Username} beo vkl",
+                Color = DiscordColor.Blue,
+            };
+
+            await context.Channel.SendMessageAsync(embed: msg);
+        }
+
+        [Command("card")]
+        public async Task CardGame(CommandContext context)
+        {
+            var userCard = new CardSystem();
+
+            var userCardEmbed = new DiscordEmbedBuilder
+            {
+                Title = $"Bai cua may la {userCard.SelectedCard} ",
+                Color = DiscordColor.Yellow,
+            };
+
+            await context.Channel.SendMessageAsync(embed: userCardEmbed);
+
+            var botCard = new CardSystem();
+
+            var botCardEmbed = new DiscordEmbedBuilder
+            {
+                Title = $"Bai cua tao la {botCard.SelectedCard}",
+                Color = DiscordColor.Lilac,
+            };
+
+            await context.Channel.SendMessageAsync(embed: botCardEmbed);
+
+            if (userCard.CardPowerNum > botCard.CardPowerNum)
+            {
+                var winMessage = new DiscordEmbedBuilder
+                {
+                    Title = "May thang roi nhe",
+                    Color = DiscordColor.Turquoise,
+                };
+
+                await context.Channel.SendMessageAsync(embed: winMessage);
+            }
+            else if (userCard.CardPowerNum == botCard.CardPowerNum && userCard.CardPowerSuits > botCard.CardPowerSuits)
+            {
+                var winMessage = new DiscordEmbedBuilder
+                {
+                    Title = "May thang roi nhe",
+                    Color = DiscordColor.Turquoise,
+                };
+
+                await context.Channel.SendMessageAsync(embed: winMessage);
+            }
+            else
+            {
+                var loseMessage = new DiscordEmbedBuilder
+                {
+                    Title = "Alexa play Loser by BigBang",
+                    Color = DiscordColor.Red,
+                };
+
+                await context.Channel.SendMessageAsync(embed: loseMessage);
+            }
+        }
+    }
+}
---
+++ other command/CardSystem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discord_App.other_command
+{
+    public class CardSystem
+    {
+        private string[] cardNum = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "Ace" };
+        private string[] cardSuits = { "Bich", "Nhep", "Ro", "Co" };
+
+        public string SelectedNumber { get; set; }
+        public string SelectedCard { get; set; }
+        public int CardPowerNum { get; set; }
+        public int CardPowerSuits { get; set; }
+        
+        public CardSystem() 
+        {
+            var random = new Random();
+            int numberIndex = random.Next(0, cardNum.Length - 1);
+            int suitIndex = random.Next(0, cardSuits.Length - 1);
+
+            this.SelectedNumber = cardNum[numberIndex];
+            this.SelectedCard = $"{cardNum[numberIndex]} {cardSuits[suitIndex]}";
+            this.CardPowerNum = numberIndex;
+            this.CardPowerSuits = suitIndex;
+
+        }
+    }
+}
